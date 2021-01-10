4a9fc9e5b26ff0a6da546381d4257c77e5e03aeb
Wednesday <yensonyu@gmail.com>
2021-01-10 12:58:31 -0800

    create orm object

---
+++ config/orm.js
@@ -0,0 +1,85 @@
+// Import MySQL connection
+var connection = require("../config/connection.js");
+
+// Helper function for passing question marks to SQL syntax
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function obToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+// Object for all SQL statement functions
+var orm = {
+    // Function to view all values
+    all: function(tableINput, cb) {
+        var queryString = "SELECT * FROM " + tableINput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // Function to insert new value
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO" + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // Funciton to update existing value
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += obToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
+
+// Export the orm object for the model
+module.exports = orm;
