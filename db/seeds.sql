d9eb19ca5c5aab3403b01f5bd2c6c370c1b57935
Wednesday <yensonyu@gmail.com>
2021-01-09 20:24:50 -0800

    insert values to db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('fish burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('beef burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name) VALUES ('sausage burger');
