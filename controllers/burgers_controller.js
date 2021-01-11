33362bb323d36ec922c084df11d9778ad4e0f982
Wednesday <yensonyu@gmail.com>
2021-01-10 17:06:36 -0800

    create controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,50 @@
+// Dependencies
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model to use its database functions
+var burger = require("../models/burger.js");
+
+// Create all routes and set up logic within those routes where required
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        }
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if(result.changeRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+// Export routes for server.js to use
+module.exports = router;
