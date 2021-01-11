64c65d299d0281233623c73ff9222933535e0ae8
Wednesday <yensonyu@gmail.com>
2021-01-10 19:20:31 -0800

    complete server

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 var express = require("express");
+
+var PORT = porcess.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory
+// app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start server so that it can begin listening to client requests
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
